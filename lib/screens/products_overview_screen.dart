fe8e2f3c3841958856c91e4c5be72c4d0de8f850
gwjun <gwjung@vdotdo.com>
2021-01-06 01:07:46 +0900

    01/06 commit

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shoppingapp/widgets/products_grid.dart';
 
+enum FilterOptions{
+  Favorites,all
+}
 
 class ProductsOverViewScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('MyShop'),),
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(icon: Icon(Icons.more_vert),
+            onSelected: (int selectedValue){
+            print(selectedValue);
+            },
+            itemBuilder: (_) => [
+            PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favorites,),
+            PopupMenuItem(child: Text('Show All'), value: FilterOptions.all,)
+          ],)
+        ],),
       body: ProductsGrid(),
     );
   }
