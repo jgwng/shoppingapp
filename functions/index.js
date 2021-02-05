c7f6fd442e9fd44bd116ed3abe8e02b3e55d3b49
zoavm <gunwng123@gmail.com>
2021-02-05 18:16:32 +0900

    02/05

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-const nodemailer = require('nodemailer');
-const cors = require('cors')({origin: true});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -9,3 +8,36 @@ const cors = require('cors')({origin: true});
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+admin.initializeApp();
+
+const msg = {
+    android : {
+           notification : {
+           },
+       },
+        apns : {
+           payload : {
+               aps : {
+                   sound : "default",
+                    alert : {
+
+                   },
+               }
+           },
+        }
+}
+
+exports.sendFCM = functions.region('asia-northeast3').https.onCall(async (data, context)  => {
+    let {title, body, target} = data;
+    msg.data.title = title;
+    msg.data.body = body;
+    msg.android.data.title = title;
+    msg.android.data.body = body;
+    // msg.apns.payload.aps.alert.title = title;
+    // msg.apns.payload.aps.alert.body = body;
+    msg.token = target
+
+    console.log(msg);
+    var result = admin.messaging().send(msg);
+    return result;
+});
