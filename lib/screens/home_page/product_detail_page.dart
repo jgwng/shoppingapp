ed6cff246ccd3e1ed08b9a671eb26daaeb0ab7f4
jgwng <gunwng123@gmail.com>
2021-03-28 01:24:59 +0900

    03/28 commit

--- lib/screens/home_page/product_detail_page.dart
+++ lib/screens/home_page/product_detail_page.dart
@@ -14,11 +14,26 @@ class ProductDetailScreen extends StatefulWidget{
 class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTickerProviderStateMixin{
   int widgetIndex = 0;
   bool isFavorite = false;
+  bool isTouched = false;
   final PageController controller = PageController(initialPage: 0);
+  ScrollController scrollController = ScrollController();
+
+  List<String> deliveryInfo = ["출고일정은 상품별 상세페이지에서 확인하실 수 있습니다.","도서산간 지역은 추가 배송비 입금 요청이 있을 수 있습니다."];
+  List<String> refundInfo = ["교환,환불 및 기타문의는 고객센터 1234-5678으로\n문의주셔야 합니다.","단순변심으로 인한 교환/환불인 경우 반송비를 입금해\n주셔야 합니다.",
+    "상품 불량인 경우 배송비를 포함한 전액이 환불됩니다.","상품 수령일로부터 7일 이내 반품/환불 접수 가능합니다.","출고 이후 환불요청 시 상품 회수 후 처리됩니다."];
+
+
+
+
+
+
+
+
+
+
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
@@ -26,32 +41,33 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: TextTitleAppBar(title : "제품 상세"),
-        body: ListView(
-          children: [
-            productInfo(),
-            StickyHeader(
-                  header: Container(
-                  height: 60.0,
-                    color: Colors.white,
+      body: ListView(
+        controller: scrollController,
+        children: [
+          productImage(),
+          StickyHeader(
+            header: Container(
+              height: 60.0,
+              color: Colors.white,
 
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment : MainAxisAlignment.spaceBetween,
-                      children: [
-                        stickyTab("상품 설명",0),
-                        stickyTab("배송/반품/고시",1),
-                        stickyTab("상품 문의",2),],
-                    ),
-                  ),
-                content: contentWidget(widgetIndex),
-    ),
+              alignment: Alignment.centerLeft,
+              child: Row(
+                mainAxisAlignment : MainAxisAlignment.spaceBetween,
+                children: [
+                  stickyTab("상품 설명",0),
+                  stickyTab("배송/반품/고시",1),
+                  stickyTab("상품 문의",2),],
+              ),
+            ),
+            content: contentWidget(widgetIndex),
+          ),
           Container(
-            height: 700,
-            color: Colors.white
+              height: 700,
+              color: Colors.white
           )
-          ],
-        ),
-        bottomNavigationBar: Container(
+        ],
+      ),
+      bottomNavigationBar: Container(
         height: 80,
         padding: EdgeInsets.symmetric(horizontal: 24,vertical: 15),
         child: Row(
@@ -83,7 +99,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
                   },
                   color: AppThemes.mainColor,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.0)
+                      borderRadius: BorderRadius.circular(6.0)
                   ),
                   child: Text("구매하기",style:AppThemes.textTheme.headline1.copyWith(color:Colors.white)),
                 ),
@@ -91,9 +107,9 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
             )
 
           ],
-        ),  
-    ),
-       );
+        ),
+      ),
+    );
 
   }
 
@@ -128,58 +144,164 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
   Widget contentWidget(int index){
     switch(index){
       case  0:
-        return Container(color: Colors.pink,height: 600);
+        return Container(height:(isTouched) ? 710 : 605,
+        child: Column(
+          children: [
+            Container(
+              height  :(isTouched) ? 605 : 500,
+              child: ListView.builder(
+              controller: scrollController,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 2,
+              shrinkWrap: false,
+              itemBuilder: (ctx,i) => Container(height: 300,child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset("assets/logo/grocery-cart.png",fit: BoxFit.contain),
+              ),),
+            ),),
+            if(!isTouched)
+            Container(
+                height: 100,
+                padding: EdgeInsets.symmetric(vertical: 25,horizontal: 30),
+                child:  RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6.0)),
+                  onPressed:(){
+                  setState(() {
+                  isTouched = true;
+                  });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("상품 설명 더 보기",style: AppThemes.textTheme.bodyText1,),
+                      SizedBox(width: 10,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.arrow_drop_down,size: 35,),
+                          SizedBox(height: 3,)
+                        ],
+                      )
+                    ],
+                  ),
+             ),
+            ),
+          ],
+        )
+
+
+
+
+
+        ,);
         break;
       case 1:
-        return Container(color: Colors.black,height: 400);
+        return Container(
+          height: 400,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              noticeInfo("배송 정보", deliveryInfo),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Divider(height: 1,thickness: 1,color: Colors.black,),),
+              noticeInfo("교환/환불/AS안내/기타",refundInfo)
+          ],
+        ),);
         break;
       case 2:
         return Container(color: Colors.green,height: 200);
         break;
-      }
-      return Container();
     }
-
+    return Container();
+  }
+  Widget noticeInfo(String title,List<String> textList){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title,style: AppThemes.textTheme.headline1,),
+        SizedBox(height: 15,),
+        Container(
+          height: title == "배송 정보" ? 50 : 200,
+          child: ListView.separated(
+            itemBuilder: (ctx,i) => noticeListItem(i,textList),
+            itemCount: textList.length,
+            controller: scrollController,
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            separatorBuilder: (ctx,i) => SizedBox(height: 10),
+          ),
+        )
+      ],
+    );
+  }
+  Widget noticeListItem(int index,List<String> textList){
+    return Row(
+      children: [
+        Container(
+          height: textList[index].contains('\n')? 25 : 10,
+          alignment: Alignment.topCenter,
+          child: Icon(Icons.fiber_manual_record,size: 8,),
+        ),
+        SizedBox(width: 5,),
+        Text(textList[index],style: AppThemes.textTheme.bodyText1.copyWith(fontSize: 14),)
+      ],
+    );
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   void _pageChanged(int index) {
     setState(() {});
   }
-    Widget productInfo(){
+  Widget productImage(){
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         NotificationListener<OverscrollIndicatorNotification>(
-        onNotification: (OverscrollIndicatorNotification overScroll) {
-          overScroll.disallowGlow();
-          return false;
-        },child :
-          Column(
-            children: [
+            onNotification: (OverscrollIndicatorNotification overScroll) {
+              overScroll.disallowGlow();
+              return false;
+            },child :
+        Column(
+          children: [
             Container(
-                  height: 350,
-                  width: 350,
-                  alignment: Alignment.center,
+              height: 350,
+              width: 350,
+              alignment: Alignment.center,
 
-                  child:PageView.builder(
-                    onPageChanged: _pageChanged,
-                    controller: controller,
-
-                    physics: ClampingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        width: 300,
-                        height: 300,
-                        child: Image.asset("assets/logo/grocery-cart.png",fit: BoxFit.fitWidth),
-                      );
-                    },
-                    itemCount: 5,
-                  ),
-                ),
-                Indicator(
+              child:PageView.builder(
+                onPageChanged: _pageChanged,
                 controller: controller,
+
+                physics: ClampingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 300,
+                    height: 300,
+                    child: Image.asset("assets/logo/grocery-cart.png",fit: BoxFit.fitWidth),
+                  );
+                },
                 itemCount: 5,
-                )
-                ],
-      )
+              ),
+            ),
+            Indicator(
+              controller: controller,
+              itemCount: 5,
+            )
+          ],
+        )
         ),
 
         SizedBox(height: 30,),
@@ -214,7 +336,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
 
       ],
     );
-    }
+  }
 
   String conversionDateTime(DateTime dateTime){
     DateTime arriveDate = DateTime(dateTime.year,dateTime.month,dateTime.day+2);
@@ -225,7 +347,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
 
 
 
-    Widget productPriceInfo(String label,String content){
+  Widget productPriceInfo(String label,String content){
     return Row(children: [
       SizedBox(
         width: 80,
@@ -234,11 +356,11 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with SingleTi
       SizedBox(width: 20,),
       Text(content,style: AppThemes.textTheme.bodyText1)
     ],);
-    }
-
-
-
-
-
   }
+
+
+
+
+
+}
 //https://flutter-examples.com/set-text-overflow-ellipsis-text-in-flutter/
